7023dd36332232658adac2c27fcfb6ad76a3d3d1
Matthew Schulman <matthewdschulman@gmail.com>
2014-04-29 00:08:49 -0400

    now identifies if a given fgets is a number

--- pcalc.c
+++ pcalc.c
@@ -3,32 +3,33 @@
 
 int main ( int argc, char *argv[] )
 {
-    if ( argc != 2 ) /* argc should be 2 for correct execution */
+	//check that the user input is correct
+    if ( argc != 2 ) /* argc should be 2 */
     {
-        /* We print argv[0] assuming it is the program name */
-        printf( "usage: %s filename", argv[0] );
+    	//error...
+    	printf("Error with the user input. Try again. Make sure to have one command line argument.");
     }
+
     else 
     {
     	//assume no command will be longer than 100 digits long
     	char curStr[100];
-        // We assume argv[1] is a filename to open
-        FILE *file = fopen( argv[1], "r" );
+        // Open up the user's selected input file
+        FILE *file = fopen(argv[1], "r");
 
-        /* fopen returns 0, the NULL pointer, on failure */
-        if ( file == 0 )
+        if (file == 0)
         {
-            printf( "Could not open file\n" );
+            printf("Could not open file\n");
         }
         else 
         {
-            /* read one character at a time from file, stopping at EOF, which
-               indicates the end of the file.  Note that the idiom of "assign
-               to a variable, check the value" used below works because
-               the assignment statement evaluates to the value assigned. */
-            while  ( (fgets(curStr, 100, file) ) != NULL )
+            // read in strings
+            while  ((fgets(curStr, 100, file)) != NULL )
             {
-                printf( "%s", curStr );
+            	int curIntIfInt = atoi(curStr);
+            	if (curIntIfInt != 0) {
+            		printf("%d", curIntIfInt);
+            	}
             }
             fclose( file );
         }
